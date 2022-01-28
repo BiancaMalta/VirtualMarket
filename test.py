7e4ecf6092849ea1d29d31363676fd7bb01898b0
Bianca Malta <92928037+BiancaMalta@users.noreply.github.com>
2022-01-28 15:29:31 -0300

    Create test.py

---
+++ test.py
@@ -0,0 +1,8 @@
+from models.product import Product
+
+xbox = Product('Microsoft Xbox One X ', 499.94)
+webcam = Product('Pro Webcam', 78.75)
+headset = Product('PC Gaming Headset', 35.99)
+print(xbox)
+print(webcam)
+print(headset)
